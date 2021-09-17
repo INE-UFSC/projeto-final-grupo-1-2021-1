08ef27f8aea88ee47453f715f27f1ad887411069
emilysk49 <emilytti@me.com>
2021-09-17 10:11:49 -0300

    Apagando atributos não usados e usando constante

--- versao_final/Entidades/camera.py
+++ versao_final/Entidades/camera.py
@@ -7,7 +7,6 @@ class Camera:
     def __init__(self, jogador):
       self.__jogador = jogador
       self.__offset = vec(0, 0)
-      self.__DISPLAY_W, self.__DISPLAY_H = 800, 500
       self.__posicao_antiga = self.__jogador.rect.x + self.__jogador.tamanho/2
       self.__borda = 0
     
@@ -18,7 +17,7 @@ class Camera:
     def scroll(self):
       if self.__jogador.rect.x + self.__jogador.tamanho/2 >= self.__posicao_antiga:
         self.__offset.x = -self.__jogador.rect.x + 400 - self.__jogador.tamanho/2
-        self.__borda = self.__jogador.rect.x - self.__DISPLAY_W/2 + self.__jogador.tamanho/2
+        self.__borda = self.__jogador.rect.x - c.largura_tela/2 + self.__jogador.tamanho/2
         self.__posicao_antiga = self.__jogador.rect.x + self.__jogador.tamanho/2
       elif self.__jogador.rect.x < self.__borda:
         self.__jogador.rect.x = self.__borda
